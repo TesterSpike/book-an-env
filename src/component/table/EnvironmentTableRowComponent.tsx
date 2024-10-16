6d666563e39a4953ae2518a1e299e7e391469deb
Sean Pike <sean.pike@globalrelay.net>
2024-10-16 17:08:46 +0100

    Remove hard-coded shareable value

--- src/component/table/EnvironmentTableRowComponent.tsx
+++ src/component/table/EnvironmentTableRowComponent.tsx
@@ -30,7 +30,7 @@ export default function EnvironmentTableRowComponent(row: EnvironmentData,
     };
 
     const bookingData = row.bookingData;
-    const shareable = (row.bookingData?.shareable) ? 'Unlocked RowItem' : 'Locked RowItem';
+    const shareable = (row.bookingData?.shareable === 'Yes') ? 'Unlocked RowItem' : 'Locked RowItem';
     const booked = (bookingData) ? 'Booked Row' : 'Row'
 
     return (
@@ -51,7 +51,7 @@ export default function EnvironmentTableRowComponent(row: EnvironmentData,
             <td className={"RowItem"}>{(bookingData) ? new Date(bookingData.bookingDate).toLocaleDateString("en-uk") : ''}</td>
             <td className={"RowItem"}>{(bookingData) ? bookingData.untilTime : ''}</td>
             <td className={"RowItem"}>{(bookingData) ? bookingData.untilTime : ''}</td>
-            <td className={`RowItemCenter ${(bookingData) ? shareable : 'RowItem'}`}>{(bookingData) ? 'Yes' : ''}</td>
+            <td className={`RowItemCenter ${(bookingData) ? shareable : 'RowItem'}`}>{(bookingData) ? bookingData.shareable : ''}</td>
             <td className={"RowItem"}>{(bookingData) ? bookingData.notes : ''}</td>
             <td className={"RowItem RowItemCenter"}>{(bookingData) ?
                 <Button id={`release-${row.env}-booking`} onClick={() => onRelease(row.env)}>Release</Button> :
